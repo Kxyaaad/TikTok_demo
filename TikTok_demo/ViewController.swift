27700f669d4e4356d3fba7b629426bf053b54c08
Kxyaaad <823942471@qq.com>
2019-10-25 14:54:57 +0800

    Initial Commit

---
+++ TikTok_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTok_demo
+//
+//  Created by SanDisk on 2019/10/25.
+//  Copyright © 2019 SanDisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
